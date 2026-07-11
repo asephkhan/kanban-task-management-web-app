007762ea36977b4eab35d9a3d73e5d7171175d64
Asif Ali <asephkhan@gmail.com>
2026-07-11 15:54:13 +0100

    created a feature which can create new board in jason server

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,24 @@ import axios from "axios";
 
 function App() {
   const [boards, setBoards] = useState([]);
+  const [newBoard, setNewBoard] = useState('a new board')
+
+  const addBoard = (e) => {
+    e.preventDefault();
+    console.log("button clicked", e.target)
+    const boardObj = {
+      name: newBoard,
+    };
+
+    axios.post("http://localhost:3001/boards", boardObj).then((response) => {
+      console.log(response, "response");
+    });
+  };
+
+  const handleBoardChange = e => {
+    console.log(event.target.value)
+    setNewBoard(e.target.value)
+  } 
 
   useEffect(() => {
     axios.get("http://localhost:3001/boards").then((response) => {
@@ -11,11 +29,19 @@ function App() {
     });
   }, []);
 
-  console.log(boards, "banana");
+  console.log(boards, "boards")
+
+  //  console.log(boards, "banana");
 
   return (
     <>
       {boards.map((board) => board.name)}
+
+      <form onSubmit={addBoard}>
+      <input value={newBoard} onChange={handleBoardChange} />
+      <button type="submit">create board</button>
+
+      </form>
     </>
   );
 }
