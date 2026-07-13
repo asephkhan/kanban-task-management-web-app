87a4011e873bb287a7237fa9b03dd7ee6f7694cb
Asif Ali <asephkhan@gmail.com>
2026-07-13 15:34:11 +0100

    added the functioality of changing the boards by clicking o them

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 function App() {
   const [boards, setBoards] = useState([]);
   const [newBoard, setNewBoard] = useState("a new board");
+  const [selectedBoard, setSelectedboard] = useState(null);
 
   const addBoard = (e) => {
     e.preventDefault();
@@ -28,51 +29,47 @@ function App() {
     });
   }, []);
 
+  if (!selectedBoard) {
+    return (
+      <>
+        {boards.map((board) => (
+          <button onClick={() => setSelectedboard(board)}>{board.name}</button>
+        ))}
+      </>
+    );
+  }
 
-  if (boards.length === 0) return <p>loading...</p>
+  if (boards.length === 0) return <p>loading...</p>;
 
-  const board = boards[0]
-//  const todo = board.columns[0].tasks.map(task => task.title)
+  const board = selectedBoard;
 
-  const columns = board.columns.map(column => column)
+  const columns = board.columns.map((column) => column);
 
-const column = columns.map(column => column.name)
+  const column = columns.map((column) => column.name);
 
-const todo = columns[0].tasks.map(todo => todo.title)
-const doing = columns[1].tasks.map(doing => doing.title)
-const done = columns[2].tasks.map(done => done.title)
-
+  const todo = columns[0].tasks.map((todo) => todo.title);
+  const doing = columns[1].tasks.map((doing) => doing.title);
+  const done = columns[2].tasks.map((done) => done.title);
 
 
-
-
-
-
-
-
-  console.log("result", done );
 
   return (
     <>
-      {boards.map((board) => board.name)}
-
       <form onSubmit={addBoard}>
         <input value={newBoard} onChange={handleBoardChange} />
         <button type="submit">create board</button>
       </form>
 
-     
-    {column} 
-    <p>todo: {todo.length}</p> 
-    <p>{todo}</p>
-    <p>doing: {doing.length}</p> 
-    <p>{doing}</p>
-    <p>done: {done.length}</p> 
-    <p>{done}</p>
-
-
-
-
+      {boards.map((board) => (
+        <button onClick={() => setSelectedboard(board)}>{board.name}</button>
+      ))}
+      {column}
+      <p>todo: {todo.length}</p>
+      <p>{todo}</p>
+      <p>doing: {doing.length}</p>
+      <p>{doing}</p>
+      <p>done: {done.length}</p>
+      <p>{done}</p>
     </>
   );
 }
